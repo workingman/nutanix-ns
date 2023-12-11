878f726d3b7a0c7caad37c9c18a436b4d5726843
kgrodrigues <34258699+kgrodrigues@users.noreply.github.com>
2023-12-11 12:18:40 -0800

    BIZAPP-0009

--- src/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
+++ src/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
@@ -12,7 +12,7 @@ define(['N/record', 'N/search'],
 
  */
 function(record, search) {
-    // 20231201-092345 gr conflicting change 20231204-1012 ; new change KR BIZAPP-0007
+    // 20231201-092345 gr conflicting change 20231204-1012 ;  KR BIZAPP-0009 conflicting change
 
     /**
      * Function to be executed after page is initialized.
@@ -46,4 +46,6 @@ function(record, search) {
     // kgr non conflicting change 20231205-1056 BIZAPP-0007 change 1
     // kgr non conflicting change 20231205-1056 BIZAPP-0007 change 2
     // kgr non conflicting change 20231205-1056 BIZAPP-0007 change 3 additional edits as requested
+    // kgr non conflicting change 20231205-1056 BIZAPP-0009
+
 });
