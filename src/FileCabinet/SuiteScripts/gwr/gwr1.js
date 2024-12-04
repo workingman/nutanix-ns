cb88dbe5a2de7b545926b4c560e3f131814b2e3a
Geoffrey Routledge <geoff@dgbi.ca>
2024-12-03 20:08:05 -0800

    update file cab

--- src/FileCabinet/SuiteScripts/gwr/gwr1.js
+++ src/FileCabinet/SuiteScripts/gwr/gwr1.js
@@ -13,7 +13,6 @@ define(['N/record', 'N/search'],
 function(record, search) {
     // GR 20240313-123224
     // GR 20240503-132414
-    // GR 20240723-095848
     /**
      * Function to be executed after page is initialized.
      *
